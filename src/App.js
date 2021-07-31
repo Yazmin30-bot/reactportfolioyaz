de223128e1ddbd713f1f325a1777e163e09a223a
Yazmin Tinoco <ytip30@gmail.com>
2021-07-31 00:46:25 -0500

    Render the page home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-/* import { BrowserRouter as Router, Route } from "react-router-dom"; */
-/* import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
+import Home from "./pages/Home";
+/*import Portfolio from "./pages/Portfolio";
 import Contact from './pages/Contact';
 import About from "./pages/About"; */
 import './App.css';
 
 function App() {
   return (
-    <div>
-      Hi Im Yazmin
-    </div>
-   /*  <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
+    
+     <Router>
+        <Route exact path="/reactportfolioyaz/" component={Home} />
+        {/* <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/about" component={About} />
-    </Router> */
+        <Route exact path="/about" component={About} /> */}
+    </Router> 
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+/* import Navbar from "../components/Navbar/Navbar";
+import AboutMe from "../components/AboutMe/AboutMe";
+import Footer from "../components/Footer/Footer"; */
+
+
+function Home() {
+    return(
+        <div className="container-fluid">
+            This is home
+            {/* <Navbar/>
+            <AboutMe/>
+            <Footer/> */}
+        </div>
+    )
+}
+
+export default Home;
