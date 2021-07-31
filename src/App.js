5acd076860b497a82b6e2642ff4245aa28a9b2a7
Yazmin Tinoco <ytip30@gmail.com>
2021-07-31 01:26:20 -0500

    Create the route to show the home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from './pages/Contact';
-/*import About from "./pages/About"; */
+import About from "./pages/About"; 
 import './App.css';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route exact path="/reactportfolioyaz/" component={Home} />
         <Route exact path="/reactportfolioyaz/portfolio" component={Portfolio} />
         <Route exact path="/reactportfolioyaz/contact" component={Contact} />
-        {/* <Route exact path="/reactportfolioyaz/about" component={About} /> */}
+        <Route exact path="/reactportfolioyaz/about" component={About} /> 
     </Router> 
   );
 }
